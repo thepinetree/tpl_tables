0e5046a1ac547c33e8d4ce258a76c61ced962fb4
Deepayan Patra <dpatra@andrew.cmu.edu>
2020-12-04 05:01:48 -0500

    Vectorwise TPCH table generation

--- gen_tpch.sh
+++ gen_tpch.sh
@@ -5,27 +5,27 @@ make
 mv *.tbl ../tables
 cd ../tables
 
-echo "Formatting..."
+#echo "Formatting..."
 # Remove last | in each line
-sed -i 's/[|]$//' part.tbl
-sed -i 's/[|]$//' supplier.tbl
-sed -i 's/[|]$//' partsupp.tbl
-sed -i 's/[|]$//' customer.tbl
-sed -i 's/[|]$//' orders.tbl
-sed -i 's/[|]$//' lineitem.tbl
-sed -i 's/[|]$//' nation.tbl
-sed -i 's/[|]$//' region.tbl
+#sed -i 's/[|]$//' part.tbl
+#sed -i 's/[|]$//' supplier.tbl
+#sed -i 's/[|]$//' partsupp.tbl
+#sed -i 's/[|]$//' customer.tbl
+#sed -i 's/[|]$//' orders.tbl
+#sed -i 's/[|]$//' lineitem.tbl
+#sed -i 's/[|]$//' nation.tbl
+#sed -i 's/[|]$//' region.tbl
 
-python gen_tpch.py part
-python gen_tpch.py supplier
-python gen_tpch.py partsupp
-python gen_tpch.py customer
-python gen_tpch.py orders
-python gen_tpch.py lineitem
-python gen_tpch.py nation
-python gen_tpch.py region
+#python gen_tpch.py part
+#python gen_tpch.py supplier
+#python gen_tpch.py partsupp
+#python gen_tpch.py customer
+#python gen_tpch.py orders
+#python gen_tpch.py lineitem
+#python gen_tpch.py nation
+#python gen_tpch.py region
 
-rm *.tbl
+#rm *.tbl
 
 # Rename to .data
 #mv part.tbl part.data
@@ -37,4 +37,4 @@ rm *.tbl
 #mv nation.tbl nation.data
 #mv region.tbl region.data
 
-echo "Formatted"
+#echo "Formatted"
